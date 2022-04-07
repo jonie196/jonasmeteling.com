f9c5a024285b51cfe15a75186d29db3b9580583f
Jonas Meteling <96886640+jonie196@users.noreply.github.com>
2022-04-07 10:49:39 -0400

    added expand function and blue body

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   sidebarMenu.classList.remove("showNav");
 });
+
 // setup booking
 const bookingBtn = document.getElementById("booking-btn");
 const booking = document.getElementById("booking");
@@ -25,3 +26,36 @@ bookingBtn.addEventListener("click", () => {
 bookingClose.addEventListener("click", () => {
   booking.classList.remove("showBooking");
 });
+
+// Read More Read Less
+function expandFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "More Details";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Show Less";
+    moreText.style.display = "inline";
+  }
+}
+// Read More Read Less
+function expandFunction2() {
+  var dots = document.getElementById("dots2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "More Details";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Show Less";
+    moreText.style.display = "inline";
+  }
+}
